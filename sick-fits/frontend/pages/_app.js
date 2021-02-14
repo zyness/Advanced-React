8597c524533f147a70f7a70bec36259575f2f30c
Vasili <wasjke@gmail.com>
2021-02-14 17:32:31 +0100

    After Lesson 19

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,8 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
+import { ApolloProvider } from '@apollo/client';
+
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
+
+import withData from '../lib/withData';
 
 import '../components/styles/nprogress.css';
 
@@ -10,10 +14,25 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
-);
+const MyApp = ({ Component, pageProps, apollo }) => {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+};
 
-export default MyApp;
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
