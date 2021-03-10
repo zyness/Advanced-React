a2cd288e7ee94032cacc304c67dcde78078f11c4
wasjke <wasjke@WASJKE-LEGION>
2021-03-10 19:12:35 +0100

    After lesson 34

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -26,8 +27,13 @@ const ProductListStyles = styled.div`
   grid-gap: 60px;
 `;
 
-const Products = () => {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+const Products = ({ page }) => {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
   if (loading) return <p>Loading ...</p>;
   if (error) return <p>Error: {error.message}</p>;
   return (
